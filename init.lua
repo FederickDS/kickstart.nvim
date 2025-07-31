f037798ccca6771a8954750b88ccb517cd35dbc4
FederickDS <federicodistefano340@gmail.com>
2025-07-31 19:08:51 +0200

    funzionamento markdown per equazioni e sezioni collassabili

--- init.lua
+++ init.lua
@@ -271,6 +271,112 @@ require('lazy').setup({
   -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
+  { --file manager
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- opzionale, per le icone
+    },
+  },
+
+  {
+    'tadmccorkle/markdown.nvim',
+    ft = 'markdown',
+    config = function()
+      require('markdown').setup {
+        -- Disable all keymaps by setting mappings field to 'false'.
+        -- Selectively disable keymaps by setting corresponding field to 'false'.
+        mappings = {
+          inline_surround_toggle = 'gs', -- (string|boolean) toggle inline style
+          inline_surround_toggle_line = 'gss', -- (string|boolean) line-wise toggle inline style
+          inline_surround_delete = 'ds', -- (string|boolean) delete emphasis surrounding cursor
+          inline_surround_change = 'cs', -- (string|boolean) change emphasis surrounding cursor
+          link_add = 'gl', -- (string|boolean) add link
+          link_follow = 'gx', -- (string|boolean) follow link
+          go_curr_heading = ']c', -- (string|boolean) set cursor to current section heading
+          go_parent_heading = ']p', -- (string|boolean) set cursor to parent section heading
+          go_next_heading = ']]', -- (string|boolean) set cursor to next section heading
+          go_prev_heading = '[[', -- (string|boolean) set cursor to previous section heading
+        },
+        inline_surround = {
+          -- For the emphasis, strong, strikethrough, and code fields:
+          -- * 'key': used to specify an inline style in toggle, delete, and change operations
+          -- * 'txt': text inserted when toggling or changing to the corresponding inline style
+          emphasis = {
+            key = 'i',
+            txt = '*',
+          },
+          strong = {
+            key = 'b',
+            txt = '**',
+          },
+          strikethrough = {
+            key = 's',
+            txt = '~~',
+          },
+          code = {
+            key = 'c',
+            txt = '`',
+          },
+        },
+        link = {
+          paste = {
+            enable = true, -- whether to convert URLs to links on paste
+          },
+        },
+        toc = {
+          -- Comment text to flag headings/sections for omission in table of contents.
+          omit_heading = 'toc omit heading',
+          omit_section = 'toc omit section',
+          -- Cycling list markers to use in table of contents.
+          -- Use '.' and ')' for ordered lists.
+          markers = { '-' },
+        },
+        -- Hook functions allow for overriding or extending default behavior.
+        -- Called with a table of options and a fallback function with default behavior.
+        -- Signature: fun(opts: table, fallback: fun())
+        hooks = {
+          -- Called when following links. Provided the following options:
+          -- * 'dest' (string): the link destination
+          -- * 'use_default_app' (boolean|nil): whether to open the destination with default application
+          --   (refer to documentation on <Plug> mappings for explanation of when this option is used)
+          follow_link = nil,
+        },
+        on_attach = nil, -- (fun(bufnr: integer)) callback when plugin attaches to a buffer
+      }
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+          vim.opt_local.foldmethod = 'expr'
+          vim.opt_local.foldexpr = "v:lua.require'markdown'.foldexpr()"
+          vim.opt_local.foldenable = true
+          vim.opt_local.foldlevel = 1
+        end,
+      })
+    end,
+  },
+
+  {
+    'plasticboy/vim-markdown',
+    ft = 'markdown',
+    init = function()
+      -- abilita il folding automatico di vim-markdown
+      vim.g.vim_markdown_folding_disabled = 0
+    end,
+    config = function()
+      -- quando apri un .md, imposta foldmethod/expr su MarkdownFold()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+          vim.opt_local.foldmethod = 'expr'
+          vim.opt_local.foldexpr = 'MarkdownFold()'
+          vim.opt_local.foldenable = true
+          -- mostra tutto all’avvio (puoi abbassare a 1 se vuoi)
+          vim.opt_local.foldlevel = 99
+        end,
+      })
+    end,
+  },
+
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -419,7 +525,6 @@ require('lazy').setup({
           },
         },
       }
-
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
@@ -944,7 +1049,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'latex' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -1011,6 +1116,7 @@ require('lazy').setup({
     },
   },
 })
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--nvim-treesitter per sintassi equazioni markdown
+-- Abilita il folding con treesitter
